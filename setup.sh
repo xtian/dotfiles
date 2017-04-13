c175e31a8b5a1ae34a5ce1d9715612a42dfffda0
Christian Wesselhoeft <cwesselhoeft@enbala.com>
2017-04-13 14:17:33 -0600

    setup.sh: Enable key repeat in Sublime

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew tap Homebrew/bundle
 brew bundle
 
+# Enable key repeat in Sublime
+defaults write com.sublimetext.3 ApplePressAndHoldEnabled -bool false
+
 # Install ruby
 ruby-install ruby stable
 gem install bundler
