fe4850b98ed429cbd2b42d3445ed608bf8d8ae8e
Christian Wesselhoeft <hi@xtian.us>
2023-01-30 10:02:14 -0700

    setup.sh: Remove bundler and rust

--- setup.sh
+++ setup.sh
@@ -5,10 +5,4 @@ brew bundle
 # Enable key repeat in Sublime
 defaults write com.sublimetext.3 ApplePressAndHoldEnabled -bool false
 
-# Install ruby
-gem install bundler
-
-# Install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 ./copy-dotfiles.sh
