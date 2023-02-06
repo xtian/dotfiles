e298d1e75749bb651c1878731b79bf2aab6ea1db
Christian Wesselhoeft <hi@xtian.us>
2023-02-06 12:55:12 -0700

    setup.sh: Fix Sublime key repeat

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 brew bundle
 
 # Enable key repeat in Sublime
-defaults write com.sublimetext.3 ApplePressAndHoldEnabled -bool false
+defaults write com.sublimetext.4 ApplePressAndHoldEnabled -bool false
 
 mkdir -p ~/.config/zsh
 git -C ~/.config/zsh/git-prompt.zsh pull || git clone --depth=1 https://github.com/woefe/git-prompt.zsh ~/.config/zsh/git-prompt.zsh
