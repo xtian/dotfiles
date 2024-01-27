16d076fefba90cc09b75bc55b2033ce5dfd0d056
Christian Wesselhoeft <hi@xtian.us>
2024-01-27 12:08:33 -0700

    setup.sh: Update nix-darwin install script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,10 @@ curl --proto '=https' --tlsv1.2 -sSf -L \
 mkdir -p ~/.config/nix-darwin
 cp ./flake.* ~/.config/nix-darwin
 
-nix run nix-darwin -- switch --flake ~/.config/nix-darwin
+nix run nix-darwin \
+  --extra-experimental-features nix-command \
+  --extra-experimental-features flakes \
+  -- switch --flake ~/.config/nix-darwin
 
 mkdir -p ~/.config/zsh
 git -C ~/.config/zsh/git-prompt.zsh pull || git clone --depth=1 https://github.com/woefe/git-prompt.zsh ~/.config/zsh/git-prompt.zsh
