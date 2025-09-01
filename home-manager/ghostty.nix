0e33339f55362e6d44ad1885b655b713f17a99a9
Christian Wesselhoeft <hi@xtian.us>
2025-09-01 11:26:46 -0600

    ghostty.nix: Set initial window size

--- home-manager/ghostty.nix
+++ home-manager/ghostty.nix
@@ -7,6 +7,8 @@
       font-family = "SF Mono Powerline";
       font-size = 15;
       theme = "dark:GruvboxDark,light:GruvboxLight";
+      window-height = 58;
+      window-width = 120;
     };
   };
 }
