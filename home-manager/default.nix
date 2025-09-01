a6039fecd58fa498c4a678af384c2e499e01e133
Christian Wesselhoeft <hi@xtian.us>
2025-09-01 11:48:48 -0600

    Change identityAgent quoting style

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -55,7 +55,7 @@
       enable = true;
       enableDefaultConfig = false;
       matchBlocks."*" = {
-        identityAgent = ''"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"'';
+        identityAgent = "'~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock'";
       };
     };
 
