feb325d70b343451adb1a935f4c475a761e2ea4d
Christian Wesselhoeft <hi@xtian.us>
2025-09-01 10:30:25 -0600

    Remove darwin.nix

--- flake.nix
+++ flake.nix
@@ -42,13 +42,32 @@
           system
           ;
       };
+
+      baseConfiguration = {
+        # Use Determinate Nix
+        nix.enable = false;
+
+        nixpkgs.config.allowUnfree = true;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = system;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        system.primaryUser = primaryUser;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+      };
     in
     {
       darwinConfigurations.${hostName} = inputs.nix-darwin.lib.darwinSystem {
         inherit system specialArgs;
 
         modules = [
-          ./darwin.nix
+          baseConfiguration
           ./darwin-system.nix
           ./homebrew.nix
           ./shell.nix
