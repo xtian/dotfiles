32bec9cd9303bafe5a0e675dbe60dbe2f1bda847
Christian Wesselhoeft <>
2025-09-15 14:54:47 -0400

    darwin-system.nix: Disable Safari config for now

--- darwin-system.nix
+++ darwin-system.nix
@@ -35,14 +35,14 @@
       "com.apple.AdLib" = {
         allowApplePersonalizedAdvertising = false;
       };
-      "com.apple.Safari" = {
-        IncludeDevelopMenu = 1;
-        PrivateBrowsingRequiresAuthentication = 1;
-        PrivateSearchEngineUsesNormalSearchEngineToggle = 1;
-        SearchProviderShortName = "DuckDuckGo";
-        ShowFullURLInSmartSearchField = 1;
-        ShowOverlayStatusBar = 1;
-      };
+      # "com.apple.Safari" = {
+      #   IncludeDevelopMenu = 1;
+      #   PrivateBrowsingRequiresAuthentication = 1;
+      #   PrivateSearchEngineUsesNormalSearchEngineToggle = 1;
+      #   SearchProviderShortName = "DuckDuckGo";
+      #   ShowFullURLInSmartSearchField = 1;
+      #   ShowOverlayStatusBar = 1;
+      # };
       "com.apple.desktopservices" = {
         DSDontWriteNetworkStores = true;
         DSDontWriteUSBStores = true;
