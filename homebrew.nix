b102a204b5016cc181511bcfccd049da77010880
Christian Wesselhoeft <hi@xtian.us>
2025-11-24 20:37:25 -0500

    homebrew.nix: Disable mas

--- homebrew.nix
+++ homebrew.nix
@@ -38,11 +38,11 @@
       "zed"
     ];
 
-    masApps = {
-      "1Password for Safari" = 1569813296;
-      "Obsidian Web Clipper" = 6720708363;
-      "Telegram" = 747648890;
-      "Wipr 2" = 1662217862;
-    };
+    # masApps = {
+    #   "1Password for Safari" = 1569813296;
+    #   "Obsidian Web Clipper" = 6720708363;
+    #   "Telegram" = 747648890;
+    #   "Wipr 2" = 1662217862;
+    # };
   };
 }
