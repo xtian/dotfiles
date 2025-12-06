20f6b839c700b1050ad76daf9e2a93922a9d2a1f
Christian Wesselhoeft <hi@xtian.us>
2025-12-06 11:27:30 -0500

    shell.nix: Configure PLUG_EDITOR

--- shell.nix
+++ shell.nix
@@ -17,6 +17,7 @@
 
     variables = {
       EDITOR = "nvim";
+      PLUG_EDITOR = "zed://file/__FILE__:__LINE__";
       SSH_AUTH_SOCK = "/Users/${primaryUser}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
     };
   };
