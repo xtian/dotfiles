6bf721da0c03bc5842042732bdc5b111f9072257
Christian Wesselhoeft <hi@xtian.us>
2025-12-21 10:29:43 -0700

    zed-editor.nix: Set soft wrapping

--- home-manager/zed-editor.nix
+++ home-manager/zed-editor.nix
@@ -22,6 +22,8 @@
       buffer_font_size = 15;
       cursor_blink = false;
       edit_predictions.mode = "subtle";
+      preferred_line_length = 140;
+      soft_wrap = "bounded";
       tab_size = 2;
       telemetry.metrics = false;
       ui_font_size = 16;
