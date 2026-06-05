1e82b169270b782e7b4ea962a47b3333147f3bff
Christian Wesselhoeft <hi@xtian.us>
2026-06-05 15:57:59 -0600

    ghostty.nix: Enable ssh-terminfo

--- home-manager/ghostty.nix
+++ home-manager/ghostty.nix
@@ -8,6 +8,7 @@
       font-size = 15;
       keybind = "shift+enter=text:\\n";
       macos-titlebar-style = "transparent";
+      shell-integration-features = "cursor,sudo,title,ssh-terminfo,ssh-env";
       theme = "dark:Gruvbox Dark,light:Gruvbox Light";
       window-height = 58;
       window-width = 120;
