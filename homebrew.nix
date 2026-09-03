affeeca93353f315f175a73cf30ac4311a108171
Christian Wesselhoeft <hi@xtian.us>
2026-09-03 10:18:57 -0600

    Re-enable MAS installation

--- homebrew.nix
+++ homebrew.nix
@@ -53,18 +53,18 @@ in
       "rectangle"
       "sublime-merge"
       "sublime-text"
-      "telegram"
       "the-unarchiver"
       "transmission"
       "vlc"
       "zed"
     ];
 
-    # masApps = {
-    #   "1Password for Safari" = 1569813296;
-    #   "Obsidian Web Clipper" = 6720708363;
-    #   "Wipr 2" = 1662217862;
-    # };
+    masApps = {
+      "1Password for Safari" = 1569813296;
+      "Obsidian Web Clipper" = 6720708363;
+      "Telegram" = 747648890;
+      "Wipr 2" = 1662217862;
+    };
   };
 
   system.activationScripts.extraActivation.text = lib.mkBefore ''
